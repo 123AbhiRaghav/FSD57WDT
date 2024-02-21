4da36668898db3fe49784d9a23a7874609907f73
123AbhiRaghav <abhiraghavd@gmail.com>
2024-02-21 20:02:37 +0530

    update

--- Objcompare.js
+++ Objcompare.js
@@ -1,8 +1,17 @@
-let Obj1 = {
-    name: "Person1",
-    age: 5
+function CompareObject(obj1,obj2){
+    let isSame = true;
+    if(Object.keys(obj1).length === Object.keys(obj2).length){
+        for(let key in obj1) {
+            console.log(key);
+            if(obj1[key] !== obj2[key]) {
+                isSame = false;
+                console.log(key + "diff value")
+            }
+        }
+    } else {
+        isSame = false;
+        console.log("object length are diff")
+    }
+    return isSame;
 }
-let Obj2 = {
-    age: 5,
-    name: "Person1"
-}
+console.log(CompareObject({name:"Person1",age:5},{age:5,name:"Person1"})? "Same" : "diff");
